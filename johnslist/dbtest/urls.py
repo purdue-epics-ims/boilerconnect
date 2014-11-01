a27c59ef489c2533291643fcd3fe92d27caf2d90
Evan Widloski <evan@Volt.(none)>
2014-11-01 08:56:30 -0400

    updated urls.py for new views

--- johnslist/dbtest/urls.py
+++ johnslist/dbtest/urls.py
@@ -1,14 +1,10 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> 224604af926632df8d37a7badb2b12cfb7e4d45b
 from django.conf.urls import patterns, url
 
 from dbtest import views
 
 urlpatterns = patterns('',
-	url(r'^organizations$', views.organization_index),
-	url(r'^jobs$', views.job_index),
-	url(r'^users$', views.user_index),
+	url(r'^user/(?P<user_id>[0-9]+)$', views.user_detail,name='user_detail'),
+	url(r'^organization/(?P<organization_id>[0-9]+)$', views.organization_detail,name='organization_detail'),
+	url(r'^job/(?P<job_id>[0-9]+)$', views.job_detail,name='job_detail'),
+	url(r'^organization/(?P<organization_id>[0-9]+)/jobs$', views.organization_job_index,name='organization_job_index'),
 	)
->>>>>>> 224604af926632df8d37a7badb2b12cfb7e4d45b
