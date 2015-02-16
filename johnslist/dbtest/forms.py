7ca963ead5171aaa06c3fc4d39b4fad60e16ffe6
MatthewChiou <mattchiou1@pal-nat184-214-32.itap.purdue.edu>
2015-02-16 11:16:46 -0500

    Revert "change Job model in models.py, forms.py"
    This reverts commit 06c2d4f6c8328bd5cc4151dc5fb8cfa15516c752.

--- johnslist/dbtest/forms.py
+++ johnslist/dbtest/forms.py
@@ -1,17 +1,2 @@
-from django.db import models
-from django.forms import ModelForm,PasswordInput
-from django.contrib.auth.models import User
-from django.contrib.auth import forms
-from .models import*
-class OrganizationCreateForm(ModelForm): 
-	        class Meta: 
-			model = Organization 
-			fields = ['name','description','categories','icon'] 
-							 
-class JobCreateForm(ModelForm): 
-	        class Meta: 
-			model = Job 
-			fields = ['name','description','duedate','request_state', 'categories']
-
-
+from django import forms
 
--- johnslist/dbtest/views.py
+++ johnslist/dbtest/views.py
@@ -5,7 +5,6 @@ from django.contrib.auth.views import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 import random
-from .forms import*
 from .decorators import user_has_object
 '''
 	user_detail - show user info
@@ -55,9 +54,9 @@ def organization_job_index(request,organization_id):
 def organization_accept_job(request,organization_id):
 	organization = Organization.objects.get(id=organization_id)
 	if request.method == 'POST':
-		job_id = Job.objects.get(id=request.POST['job_id'])
-		jobstate = JobState.objects.get(job = job_id,responsible_group = organization)
-		jobstate.accepted = True
+		job = Job.objects.get(id=request.POST['job_id'])
+		job.accepted.add(organization)
+		job.requested.remove(organization)
 		return render(request, 'dbtest/confirm.html',{'title':'Job acceptance','message':'You have accepted the job: {0}'.format(job.name)})
 
 	jobs = organization.requested.all()
@@ -205,6 +204,7 @@ def job_create(request):
 			job = form.save(commit=False)
 			job.creator = User.objects.get(id=1)
 			#create new org
+			job.accepted = 0;
 			job.save()
 			form.save_m2m()
 			title = "Job {0} created".format( job.name )
