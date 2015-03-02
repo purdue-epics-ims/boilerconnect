b1035bb85fccda6536c1322505e5d4400d8035b5
Evan <evan@evanw.org>
2015-03-02 12:13:28 -0500

    Revert "Merge branch 'develop' of http://github.com/purdue-epics-ims/ims2014 into develop"
    This reverts commit c7a6f58e56d61a79d7f9316333ce7bdb91e08d65, reversing
    changes made to a32d6705f9a8345c1d19b4f515135af83a9d8b3e.

--- johnslist/dbtest/admin.py
+++ johnslist/dbtest/admin.py
@@ -3,5 +3,5 @@ from .models import *
 
 # Register your models here.
 admin.site.register(Job)
-admin.site.register(Category)
+admin.site.register(ServiceCategory)
 admin.site.register(Organization)
