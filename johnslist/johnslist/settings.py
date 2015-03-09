b1d5b85a550b7563e72aafa4f0c2db488bb8e685
MatthewChiou <mattchiou1@pal-nat184-208-86.itap.purdue.edu>
2015-03-09 10:38:10 -0400

    add content processor in setting.py

--- johnslist/johnslist/settings.py
+++ johnslist/johnslist/settings.py
@@ -104,3 +104,11 @@ LOGIN_REDIRECT_URL = '/'
 
 # generic redirect url
 REDIRECT_URL = '/'
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+	"django.core.context_processors.auth",
+        "django.core.context_processors.debug",
+	"django.core.context_processors.i18n",
+	"django.core.context_processors.media",
+	"django.core.context_processors.request",
+)
