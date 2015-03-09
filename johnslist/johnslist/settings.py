6f1b1d9b60d95bdb828e2448b84bd556c4fe1827
Evan <evan@evanw.org>
2015-03-09 11:55:13 -0400

    Revert "Revert "update content processor setting""
    This reverts commit 6a5a7e4761bdd828e3653b9f833d50bd9f7a18e7.

--- johnslist/johnslist/settings.py
+++ johnslist/johnslist/settings.py
@@ -7,7 +7,6 @@ https://docs.djangoproject.com/en/1.7/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.7/ref/settings/
 """
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
@@ -106,7 +105,7 @@ LOGIN_REDIRECT_URL = '/'
 REDIRECT_URL = '/'
 
 TEMPLATE_CONTEXT_PROCESSORS = (
-	"django.core.context_processors.auth",
+	"django.contrib.auth.context_processors.auth",
         "django.core.context_processors.debug",
 	"django.core.context_processors.i18n",
 	"django.core.context_processors.media",
