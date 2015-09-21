ae09676aea2739a04b8f485b1bfce6cf3ec79aff
mmalesev <mmalesev@purdue.edu>
2015-09-21 12:06:21 -0400

    adding imports in qshell

--- johnslist/dbtest/management/commands/qshell.py
+++ johnslist/dbtest/management/commands/qshell.py
@@ -1,12 +1,16 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.core.management import execute_from_command_line
 import code
+from django.core.urlresolvers import reverse
+from django.test import Client
+
 class Command(BaseCommand):
     help = "Imports models and guardian for testing. Also stores a user instance 'u', organization 'o', category 'c', and group 'g' for testing purposes."
     def handle(self, *args, **options):
         from guardian.shortcuts import assign_perm
         from dbtest.models import User,Organization,Job,Category,Group,Jobrelation
-	u = User.objects.get(id=2)
+	u = User.objects.get(id=1)
+   client = Client()
 	o = Organization.objects.get(id=1)
 	j = Job.objects.get(id=1)
 	c = Category.objects.get(id=2)
