7120d649d46b8cbe7a1b288d0d5dd282ebeac41c
mmalesev <mmalesev@purdue.edu>
2015-09-27 00:31:54 -0400

    fixing qshell with adding lines for unittests

--- johnslist/dbtest/management/commands/qshell.py
+++ johnslist/dbtest/management/commands/qshell.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.core.management import execute_from_command_line
 import code
+from django.utils import unittest
 from django.core.urlresolvers import reverse
 from django.test import Client
 
@@ -9,10 +10,10 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         from guardian.shortcuts import assign_perm
         from dbtest.models import User,Organization,Job,Category,Group,Jobrelation
-	u = User.objects.get(id=1)
-   client = Client()
-	o = Organization.objects.get(id=1)
-	j = Job.objects.get(id=1)
-	c = Category.objects.get(id=2)
-	g = Group.objects.get(id=1)
-	code.interact(local=locals())
+        u = User.objects.get(id=1)
+        client = Client()
+        o = Organization.objects.get(id=1)
+        j = Job.objects.get(id=1)
+        c = Category.objects.get(id=2)
+        g = Group.objects.get(id=1)
+        code.interact(local=locals())
