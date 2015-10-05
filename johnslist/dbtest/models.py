9e9804104f43571f0dfc728c39f1e2c082499833
mmalesev <mmalesev@purdue.edu>
2015-10-04 22:47:02 -0400

    icon field for creating an org is not mandatory any more

--- johnslist/dbtest/models.py
+++ johnslist/dbtest/models.py
@@ -23,7 +23,7 @@ class Organization(models.Model):
     email = models.CharField('Organization email',max_length=64,null=True)
     group = models.OneToOneField(Group) # Organization - Group
     phone_number = models.CharField('Organization phone number',max_length=64,null=True)
-    icon = models.ImageField(upload_to='organization',null=True)
+    icon = models.ImageField(upload_to='organization',null=True, blank=True)
     
     def job_accepted(self):
         job_list_a = Job.objects.filter(jobrelation__organization = self,jobrelation__accepted = True,jobrelation__completed = False)    
