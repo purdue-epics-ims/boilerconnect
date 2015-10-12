975da75c6e8cf83c64e516e870ce5fbf77604b4b
Evan Widloski <evan@evanw.org>
2015-10-12 15:09:52 -0400

    determine model in user_has_perm from get_perms_for_model

--- johnslist/dbtest/decorators.py
+++ johnslist/dbtest/decorators.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from guardian.shortcuts import get_perms_for_model
 from johnslist.settings import REDIRECT_URL
 from .models import *
 
@@ -11,19 +12,36 @@ def user_has_perm(perm):
             user = request.user
 
             #check if user has perm for Organization
-            if 'organization_id' in kwargs.keys():
+
+            # if 'organization_id' in kwargs.keys():
+            if perm in [p.codename for p in get_perms_for_model(Organization)]:
+                print 'a'
                 organization = Organization.objects.get(id=kwargs['organization_id'])
                 if user.has_perm(perm,organization):
                     success = True
 
             #or, check if user has perm for Job
-            elif 'job_id' in kwargs.keys():
+
+            # elif 'job_id' in kwargs.keys():
+            if perm in [p.codename for p in get_perms_for_model(Job)]:
+                print 'b'
                 job = Job.objects.get(id=kwargs['job_id'])
                 if user.has_perm(perm,job):
                     success = True
 
+            # elif 'job_id' in kwargs.keys():
+            if perm in [p.codename for p in get_perms_for_model(Jobrelation)]:
+                print 'c'
+                job = Job.objects.get(id=kwargs['job_id'])
+                jobrelation = Jobrelation.objects.get(job_id = kwargs['job_id'],organization_id = kwargs['organization_id'])
+                if user.has_perm(perm,jobrelation):
+                    success = True
+
             #or, check if user has perm for User
+
+            #no permissions on user objects right now
             elif 'user_id' in kwargs.keys():
+                print 'd'
                 user = User.objects.get(id=kwargs['user_id'])
                 if request.user == user:
                     success = True
