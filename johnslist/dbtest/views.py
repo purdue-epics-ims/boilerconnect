fa29a61b0f5e75e68835a769b982d4e354a873f7
matthewchiou <mchiou@purdue.edu>
2016-02-21 17:11:52 -0500

    check user has perm to edit jobrequest state in jobrequest_dash.html

--- johnslist/dbtest/views.py
+++ johnslist/dbtest/views.py
@@ -132,22 +132,22 @@ def jobrequest_dash(request,job_id,organization_id):
     organization = Organization.objects.get(id=organization_id)
     jobrequest = JobRequest.objects.get(job = job, organization = organization)
     comment_text = jobrequest.comment_set.all()
-    permission_to_edit =  request.user.has_perm('edit_jobrequest_state',jobrequest)
+    permission_to_edit = request.user.has_perm('edit_jobrequest_state',jobrequest)
     # if request is a POST
     if request.method == 'POST':
         form = CommentCreateForm(request.POST)
         if request.POST.get("action","")=="Accept Request":
             if jobrequest.is_pending():
                 jobrequest.accept()
-                return render(request, 'dbtest/jobrequest_dash.html',{permission_to_edit:'permission_to_edit','comment_text':comment_text,'jobrequest':jobrequest,'confirm':'You have accepted this job.'})
+                return render(request, 'dbtest/jobrequest_dash.html',{'permission_to_edit':permission_to_edit,'comment_text':comment_text,'jobrequest':jobrequest,'confirm':'You have accepted this job.'})
             else:
-                return render(request,'dbtest/jobrequest_dash.html',{permission_to_edit:'permission_to_edit','comment_text':comment_text,'jobrequest':jobrequest,'error':'You have already accepted/declined the job'})
+                return render(request,'dbtest/jobrequest_dash.html',{'permission_to_edit':permission_to_edit,'comment_text':comment_text,'jobrequest':jobrequest,'error':'You have already accepted/declined the job'})
         if request.POST.get("action","")=="Decline Request":
             if jobrequest.is_pending():
                 jobrequest.decline()
-                return render(request, 'dbtest/jobrequest_dash.html',{permission_to_edit:'permission_to_edit','comment_text':comment_text,'jobrequest':jobrequest,'confirm':'You have declined this job.'})
+                return render(request, 'dbtest/jobrequest_dash.html',{'permission_to_edit':permission_to_edit,'comment_text':comment_text,'jobrequest':jobrequest,'confirm':'You have declined this job.'})
             else:
-                return render(request,'dbtest/jobrequest_dash.html',{permission_to_edit:'permission_to_edit','comment_text':comment_text,'jobrequest':jobrequest,'error':'you have already accepted/declined this job'})
+                return render(request,'dbtest/jobrequest_dash.html',{'permission_to_edit':permission_to_edit,'comment_text':comment_text,'jobrequest':jobrequest,'error':'you have already accepted/declined this job'})
         if form.is_valid():
             comment = form.save(commit = False)
             comment.creator = request.user
@@ -172,7 +172,7 @@ def jobrequest_dash(request,job_id,organization_id):
 
     # if request is GET
     return render(request, 'dbtest/jobrequest_dash.html',
-                  {'jobrequest':jobrequest,'comment_text':comment_text,'show_dialog':show_dialog})
+            {'jobrequest':jobrequest,'comment_text':comment_text,'show_dialog':show_dialog,'permission_to_edit':permission_to_edit})
 
 #load the front page with 3 random organizations in the gallery
 def front_page(request):
