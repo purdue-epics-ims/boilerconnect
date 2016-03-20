1aadde4d1774182753f0e09ed1e742a55c58254c
evan <evan@evanw.org>
2016-03-20 16:26:21 -0400

    update deprecated context_processor

--- johnslist/johnslist/settings.py
+++ johnslist/johnslist/settings.py
@@ -65,34 +65,25 @@ ROOT_URLCONF = 'johnslist.urls'
 
 WSGI_APPLICATION = 'johnslist.wsgi.application'
 
-TEMPLATES = [
-{
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-                # insert your TEMPLATE_DIRS here
-
-                        ],
-        'APP_DIRS': True,
-        'OPTIONS': {
-                'context_processors': [
-                        # Insert your TEMPLATE_CONTEXT_PROCESSORS here or use this
-                        # list if you haven't customized them:
-                        'django.contrib.auth.context_processors.auth',
-                        'django.template.context_processors.debug',
-                        'django.template.context_processors.i18n',
-                        'django.template.context_processors.media',
-                        'django.template.context_processors.static',
-                        'django.template.context_processors.tz',
-                        'django.contrib.messages.context_processors.messages',
-                        'django.core.context_processors.request',
-
-                                        ],
-
-                        },
-
-        },
-
-]
+TEMPLATES = [{
+    'BACKEND': 'django.template.backends.django.DjangoTemplates',
+    'DIRS': [],
+    'APP_DIRS': True,
+    'OPTIONS': {
+        'context_processors': [
+            # Insert your TEMPLATE_CONTEXT_PROCESSORS here or use this
+            # list if you haven't customized them:
+            'django.contrib.auth.context_processors.auth',
+            'django.template.context_processors.debug',
+            'django.template.context_processors.i18n',
+            'django.template.context_processors.media',
+            'django.template.context_processors.static',
+            'django.template.context_processors.tz',
+            'django.contrib.messages.context_processors.messages',
+            'django.template.context_processors.request',
+        ],
+    },
+},]
 
 
 # Database
