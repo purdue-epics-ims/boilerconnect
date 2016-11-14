d4a07a703161ed40b8a907b96a10f2f8542263ca
evan <evan@evanw.org>
2016-11-14 12:10:54 -0500

    added some filters to django admin

--- johnslist/dbtest/admin.py
+++ johnslist/dbtest/admin.py
@@ -2,6 +2,34 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(Job)
-admin.site.register(Category)
-admin.site.register(Organization)
+
+class JobAdmin(admin.ModelAdmin):
+    model = Job
+    list_filter = ('organizations__name',
+                   'closed',
+                   'categories',
+                   'duedate',
+    )
+    list_display = ('name',
+                    'closed',
+                    'duedate',
+    )
+
+class CategoryAdmin(admin.ModelAdmin):
+    model = Category
+    list_filter = ('group__name',)
+
+class OrganizationAdmin(admin.ModelAdmin):
+    model = Organization
+    list_filter = ('categories',
+                   'available',
+    )
+    list_display = ('name',
+                    'available',
+    )
+
+
+admin.site.register(Job,JobAdmin)
+admin.site.register(Category,CategoryAdmin)
+admin.site.register(CategoryGroup)
+admin.site.register(Organization,OrganizationAdmin)
