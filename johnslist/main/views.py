50731bbee922570d5d929e057050b3b93be4396e
Nivedita Nighojkar <nnighojk@purdue.edu>
2018-05-07 14:56:14 -0400

    Add files via upload

--- johnslist/main/views.py
+++ johnslist/main/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render,get_object_or_404,redirect
+from django.shortcuts import render,get_object_or_404,redirect
 from .models import *
+from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.contrib.auth.views import login as auth_login
 from django.contrib.auth import authenticate
@@ -14,6 +16,9 @@ from notifications import notify
 from django.core.mail import send_mail
 from itertools import chain
 from django.contrib import messages
+from django.http import JsonResponse
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect
 
 def quicksearch(request):
     orgs = Organization.objects.all()
@@ -450,13 +455,60 @@ def job_settings(request,job_id):
         form = JobEditForm(request.POST, instance=job)
 
         #check form validity
-        if form.is_valid() :
+        if form.is_valid():
             #get form info
             job = form.save(request)
-
             #add new orgs/remove removed orgs here
 
             message = "Job {0} has been modified.".format(job.name)
             messages.add_message(request, messages.INFO, message)
 
     return render(request, 'main/job_settings.html', {'form':form,'job' : job})
+
+@login_required
+def job_status_update(request):
+    status = request.GET['status']
+    job_id = request.GET['Jobid'] 
+    if status == 'Active':
+        flag = True
+    else:
+        flag = False
+    job = Job.objects.get(pk=job_id)
+    try:
+        #job = Job.objects.get(pk=job_id)
+        job.active = flag
+        job.save()
+        #write back
+        return HttpResponse(status)
+    except Exception as e:
+        return JsonResponse(status)
+    #return render(request, 'main/job_dash.html', {'form':form,'job' : job})
+
+# for aprroval of project on admin side when it is made
+@login_required
+def job_approve_update(request):
+    status = request.GET['approve']
+    job_id = request.GET['Jobid'] 
+    if approve == 'Yes':
+        flag = True
+    else:
+        flag = False
+    job = Job.objects.get(pk=job_id)
+    try:
+        job.approve = flag
+        job.save()
+        return HttpResponse(status)
+    except Exception as e:
+        return JsonResponse(status)
+   
+
+@login_required
+def delete_job(request):
+     job_id = request.GET['Jobid']
+     job = Job.objects.get(pk=job_id)
+     try:
+         job.delete()
+         return JsonResponse({'url':'/user'}) 
+     except Exception as e:
+         return HttpResponse("deletion not successful")
+     
